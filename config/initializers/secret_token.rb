063a50b6e393b010cd09cb43fbff0d8b82fb25af
Guy Archibald <goarchib83@gmail.com>
2013-06-13 15:03:31 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Careli::Application.config.secret_token = '6df43f3d3e44afb43029498dd9359bdd3f54b4a49a65dff9012d1fb4331188eefe502cbda0042b71f3a9a637683fea65d797ca0ec7af719d12f8c708014e1148'
